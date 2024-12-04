eeb7aa96ac84c3711293264e914ec300f8a26f80
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-12-04 14:18:31 +0700

    input output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+  import { FlowbiteService } from './services/flowbite.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lp_angular';
+
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  dummyData = {
+    title: 'My first project',
+    content: 'This is my personal angular app proj',
+  }
+
+  internalMessage: string = 'This is my internal message app.component';
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
+
+  receiptMessage($message: string) : void {
+    this.internalMessage = $message;
+  }
+
 }
